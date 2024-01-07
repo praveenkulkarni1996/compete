d71e280f41474700974fde554e6a178614d36911
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2024-01-07 19:26:28 +0530

    Add mint to library code.

--- snippets/init.cpp
+++ snippets/init.cpp
@@ -6,9 +6,15 @@
 using namespace std;
 using int64 = long long;
 
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template <typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
+
   $1
 }
---
+++ snippets/mint.cpp
@@ -0,0 +1,119 @@
+template <unsigned int M>
+struct modint {
+  int64 x;
+
+  modint(const modint<M> &m) : x(m.x) {}
+  explicit modint(const int64 _x) : x((_x >= 0 ? _x % M : M + (_x % (int)M))) {}
+  explicit modint(const int _x) : x((_x >= 0 ? _x % M : M + (_x % (int)M))) {}
+  explicit modint() : x(0) {}
+
+  [[nodiscard]] modint<M> inv() const { return power(M - 2); }
+
+  [[nodiscard]] modint<M> power(int64 exp) const {
+    modint<M> res(1), base(x);
+    while (exp) {
+      if (exp & 1) res *= base;
+      base *= base;
+      exp >>= 1;
+    }
+    return res;
+  }
+
+  [[nodiscard]] modint<M> power(modint<M> &exp) const { return power(exp.x); }
+
+  modint<M> &operator=(const int _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const int64 _x) {
+    x = (_x >= 0 ? _x % M : M + (_x % M));
+    return *this;
+  }
+
+  modint<M> &operator=(const modint<M> &other) {
+    x = other.x;
+    return *this;
+  }
+
+  modint<M> &operator+=(const modint<M> &other) {
+    x += other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator-=(const modint<M> &other) {
+    x += M - other.x;
+    x -= (x >= M ? M : 0);
+    return *this;
+  }
+
+  modint<M> &operator*=(const modint<M> &other) {
+    x *= other.x;
+    x %= M;
+    return *this;
+  }
+
+  modint<M> &operator/=(const modint<M> &other) {
+    x = (*this * other.inv()).x;
+    return *this;
+  }
+
+  friend modint<M> operator+(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) += rhs);
+  }
+
+  friend modint<M> operator-(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) -= rhs);
+  }
+
+  friend modint<M> operator*(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) *= rhs);
+  }
+
+  friend modint<M> operator/(const modint<M> &lhs, const modint<M> &rhs) {
+    return (modint(lhs) /= modint(rhs));
+  }
+
+  friend bool operator==(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x == rhs.x);
+  }
+
+  friend bool operator!=(const modint<M> &lhs, const modint<M> &rhs) {
+    return (lhs.x != rhs.x);
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const modint<M> &m) {
+    return (os << m.x);
+  }
+
+  friend std::istream &operator>>(std::istream &in, modint<M> &m) {
+    return (in >> m.x);
+  }
+
+  modint<M> &operator++() {
+    x++;
+    x = (x == M ? 0 : x);
+    return *this;
+  }
+
+  modint<M> &operator--() {
+    x = (x == 0 ? M : x);
+    x--;
+    return *this;
+  }
+
+  modint<M> operator++(int) {
+    modint result = *this;
+    ++*this;
+    return result;
+  }
+
+  modint<M> operator--(int) {
+    modint result = *this;
+    --*this;
+    return result;
+  }
+};
+
+using mint = modint<kMod>;
