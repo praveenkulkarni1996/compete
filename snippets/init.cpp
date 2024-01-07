c53332cc7190357d8e48757eed6a33196c249f90
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2024-01-07 19:07:36 +0530

    Style change init.cpp

--- snippets/init.cpp
+++ snippets/init.cpp
@@ -1,10 +1,10 @@
 #include "bits/stdc++.h"
-using namespace std;
-using int64 = long long;
-using vi = vector<int>;
 #ifndef LOCAL
 #define debug(...) 42
 #endif
+
+using namespace std;
+using int64 = long long;
 
 int main() {
   ios_base::sync_with_stdio(false);
